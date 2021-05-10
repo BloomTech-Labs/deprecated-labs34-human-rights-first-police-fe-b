7876e1d8eb0200a516533e9b751ce389b6187c83
zempo <zelenkosolomon@gmail.com>
2021-05-10 15:13:00 -0700

    Fix header hierarchy on IncidentsCard.js. Remove redundant css

--- src/components/incidents/IncidentsCard.js
+++ src/components/incidents/IncidentsCard.js
@@ -9,17 +9,17 @@ const IncidentsCard = props => {
   let cityState = `${props.incident.city}, ${props.incident.state}`;
 
   return (
-    <div className="card-box">
+    <li className="card-box">
       <Card bordered={false}>
-        <div className="card-title">
-          <h4>
+        <h3 className="incident-title">{props.incident.title}</h3>
+        <div className="card-metadata">
+          <p>
             {DateTime.fromISO(props.incident.date)
               .plus({ days: 1 })
               .toLocaleString(DateTime.DATE_MED)}
-          </h4>
-          <h4>{cityState}</h4>
+          </p>
+          <p>{cityState}</p>
         </div>
-        <h3 className="incident-title">{props.incident.title}</h3>
         <h5 className="card-force">{props.incident.force_rank}</h5>
         <div className="tag-container">
           {props.incident.categories.map(cat => {
@@ -40,7 +40,7 @@ const IncidentsCard = props => {
           </Button>
         </Popover>
       </Card>
-    </div>
+    </li>
   );
 };
 
